8812039c9995f6148ae191ead3029a339f2566db
Eric Driussi <ericdriussi@gmail.com>
2025-02-03 11:03:42 +0000

    add todos and skip ufw

--- init.sh
+++ init.sh
@@ -3,10 +3,11 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# TODO: add install with dnf and pacman as well
 if [ "$EUID" -eq 0 ]; then # is root user
-    apt install -y pipx git openssh-client
+  apt install -y pipx git openssh-client
 else
-    sudo apt install -y pipx git openssh-client
+  sudo apt install -y pipx git openssh-client
 fi
 
 pipx install --include-deps ansible
@@ -14,13 +15,13 @@ pipx install --include-deps ansible
 directory="./HYO"
 mkdir -p "$directory"
 if [ -d "$directory/.git" ]; then # if was already cloned
-    cd "$directory" && git "$directory" pull
+  cd "$directory" && git "$directory" pull
 else
-    # clone the repository preserving previous files (.env.yml from ci)
-    git clone https://gitlab.com/ericdriussi/host-your-own.git /tmp/"$directory"
-    cp -ir /tmp/"$directory"/.git "$directory"/.git
-    cd "$directory" && git restore .
+  # clone the repository preserving previous files (.env.yml from ci)
+  git clone https://gitlab.com/ericdriussi/host-your-own.git /tmp/"$directory"
+  cp -ir /tmp/"$directory"/.git "$directory"/.git
+  cd "$directory" && git restore .
 fi
 
-git checkout ci
+git checkout ci #TODO: rm checkout
 ~/.local/bin/ansible-playbook run.yml "$@"
