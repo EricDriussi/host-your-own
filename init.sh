69aa52008b234ee8ef7436ef08acfebfd7f5e921
Eric Driussi <ericdriussi@gmail.com>
2025-02-07 12:18:02 +0000

    todos and refactor

--- init.sh
+++ init.sh
@@ -4,33 +4,21 @@ set -o errexit
 set -o pipefail
 
 if command -v apt &>/dev/null; then
-  install_cmd="apt install -y pipx git openssh-client"
+  install_pipx="apt install -y pipx"
 elif command -v dnf &>/dev/null; then
-  install_cmd="dnf install -y pipx git openssh-client"
+  install_pipx="dnf install -y pipx"
 elif command -v pacman &>/dev/null; then
-  install_cmd="pacman --noconfirm -S python-pipx git openssh-client"
+  install_pipx="pacman --noconfirm -S python-pipx"
 else
   echo "Unsupported package manager."
   exit 1
 fi
 
-if [ "$EUID" -eq 0 ]; then # is root user
-  eval "$install_cmd"
+if [ "$EUID" -eq 0 ]; then # is root user (ci)
+  eval "$install_pipx"
 else
-  eval sudo "$install_cmd"
+  eval sudo "$install_pipx"
 fi
 
 pipx install --include-deps ansible
-
-directory="./HYO"
-mkdir -p "$directory"
-if [ -d "$directory/.git" ]; then # if was already cloned
-  cd "$directory" && git "$directory" pull
-else
-  # clone the repository preserving previous files (.env.yml from ci)
-  git clone https://gitlab.com/ericdriussi/host-your-own.git /tmp/"$directory"
-  cp -ir /tmp/"$directory"/.git "$directory"/.git
-  cd "$directory" && git restore .
-fi
-
 ~/.local/bin/ansible-playbook run.yml "$@"
