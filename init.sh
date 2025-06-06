e61affc15e5c93d5dcaa1231c016081bc4b9d6a0
Tristan WAGNER <t.wagner66250@gmail.com>
2025-06-07 01:08:59 +0200

    support brew
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

--- init.sh
+++ init.sh
@@ -9,12 +9,16 @@ elif command -v dnf &>/dev/null; then
   install_pipx="dnf install -y pipx"
 elif command -v pacman &>/dev/null; then
   install_pipx="pacman --noconfirm -S python-pipx"
+elif command -v brew &>/dev/null; then
+  install_pipx="brew install pipx"
 else
   echo "Unsupported package manager."
   exit 1
 fi
 
-if [ "$EUID" -eq 0 ]; then # is root user (ci)
+if
+  [ "$EUID" -eq 0 ] || [ -n "$(command -v brew)" ]
+then # is root user (ci) or brew used
   eval "$install_pipx"
 else
   eval sudo "$install_pipx"
